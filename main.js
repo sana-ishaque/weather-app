9f774818bcf8f72e6f7e586d441712855dd219f6
sana-ishaque <49162351+sana-ishaque@users.noreply.github.com>
2019-09-03 15:56:30 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-
 let bodyElement = document.querySelector("body");
+
 function night() {
     bodyElement.classList.remove("day");
     bodyElement.classList.add("night");
@@ -22,7 +22,7 @@ function getWeather() {
 
     // Make a request for a user with a given ID
     axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=12e844e9b297084077b1b9af18b8babb`)
-        .then(function (response) {
+        .then(function(response) {
             // handle success
 
             console.log(response);
@@ -30,37 +30,37 @@ function getWeather() {
             document.querySelector('#temp').innerHTML = `${Math.floor(response.data.main.temp - 273.15)}°C `;
             document.querySelector('#city').innerHTML = `${response.data.name}, ${response.data.sys.country}`;
             if (response.data.weather[0].main === "Clouds" && response.data.weather[0].description === "overcast clouds") {
-                document.querySelector('#weather').innerHTML = `<img class="outsource" src="/icon/Overcast.png">`;
+                document.querySelector('#weather').innerHTML = `<img class="outsource" src="./icon/Overcast.png">`;
                 document.querySelector('#main').innerHTML = (response.data.weather[0].description);
             } else if (response.data.weather[0].main === "Clouds") {
-                document.querySelector('#weather').innerHTML = `<img class="outsource" src="/icon/iconfinder_weather-2_1214004.svg">`;
+                document.querySelector('#weather').innerHTML = `<img class="outsource" src="./icon/iconfinder_weather-2_1214004.svg">`;
                 document.querySelector('#main').innerHTML = (response.data.weather[0].description);
             } else if (response.data.weather[0].main === "Rain") {
-                document.querySelector('#weather').innerHTML = `<img class="outsource" src="/icon/rain.png">`;
+                document.querySelector('#weather').innerHTML = `<img class="outsource" src="./icon/rain.png">`;
                 document.querySelector('#main').innerHTML = (response.data.weather[0].description);
             } else if (response.data.weather[0].main == "Clear") {
-                document.querySelector('#weather').innerHTML = `<img class="outsource" src="/icon/Sunny.png">`;
+                document.querySelector('#weather').innerHTML = `<img class="outsource" src="./icon/Sunny.png">`;
                 document.querySelector('#main').innerHTML = (response.data.weather[0].description);
             } else if (response.data.weather[0].main == "Fog" || response.data.weather[0].main == "Mist" || response.data.weather[0].main == "Smoke") {
-                document.querySelector('#weather').innerHTML = `<img class="outsource" src="/icon/Fog.png">`;
+                document.querySelector('#weather').innerHTML = `<img class="outsource" src="./icon/Fog.png">`;
                 document.querySelector('#main').innerHTML = (response.data.weather[0].description);
             } else if (response.data.weather[0].main == "Haze") {
-                document.querySelector('#weather').innerHTML = `<img class="outsource" src="/icon/hazy.png">`;
+                document.querySelector('#weather').innerHTML = `<img class="outsource" src="./icon/hazy.png">`;
                 document.querySelector('#main').innerHTML = (response.data.weather[0].description);
             } else if (response.data.weather[0].main == "Thunderstorm") {
-                document.querySelector('#weather').innerHTML = `<img class="outsource"  src="/icon/storm.svg">`;
+                document.querySelector('#weather').innerHTML = `<img class="outsource"  src="./icon/storm.svg">`;
                 document.querySelector('#main').innerHTML = (response.data.weather[0].description);
             } else if (response.data.weather[0].main == "Drizzle") {
-                document.querySelector('#weather').innerHTML = `<img class="outsource" src="/icon/drizzle.png">`;
+                document.querySelector('#weather').innerHTML = `<img class="outsource" src="./icon/drizzle.png">`;
                 document.querySelector('#main').innerHTML = (response.data.weather[0].description)
             } else if (response.data.weather[0].main == "Sleet") {
-                document.querySelector('#weather').innerHTML = `<img class="outsource" src="/icon/Sleet.png">`;
+                document.querySelector('#weather').innerHTML = `<img class="outsource" src="./icon/Sleet.png">`;
                 document.querySelector('#main').innerHTML = (response.data.weather[0].description)
             } else if (response.data.weather[0].main == "Hail") {
-                document.querySelector('#weather').innerHTML = `<img class="outsource" src="/icon/Hail_Heavy.png">`;
+                document.querySelector('#weather').innerHTML = `<img class="outsource" src="../icon/Hail_Heavy.png">`;
                 document.querySelector('#main').innerHTML = (response.data.weather[0].description)
             } else if (response.data.weather[0].main == "Snow") {
-                document.querySelector('#weather').innerHTML = `<img class="outsource" src="/icon/snow.png">`;
+                document.querySelector('#weather').innerHTML = `<img class="outsource" src="./icon/snow.png">`;
                 document.querySelector('#main').innerHTML = (response.data.weather[0].description)
             }
             document.querySelector('#humidity').innerHTML = ` Humidity :  ${response.data.main.humidity}`;
@@ -69,11 +69,11 @@ function getWeather() {
 
 
         })
-        .catch(function (error) {
+        .catch(function(error) {
             // handle error
             console.log(error);
         })
-        .finally(function () {
+        .finally(function() {
             // always executed
         });
 }
